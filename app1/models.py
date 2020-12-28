0a6abe6cee81ac459c861ce5a2621096f2abc2ca
rs <rs@gmail.com>
2020-12-28 17:14:06 +0530

    added favorite functionality

--- app1/models.py
+++ app1/models.py
@@ -14,6 +14,7 @@ class Albums(models.Model):
 class Song(models.Model):
     album = models.ForeignKey(Albums, on_delete=models.CASCADE)
     song_title = models.CharField(max_length=200)
+    is_favorite = models.BooleanField(default=False)
 
     def __str__(self):
         return self.song_title
--- app1/urls.py
+++ app1/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id>/', views.details, name='details'),
+    path('<int:id>/favorite', views.favorite, name='favorite')
 ]
