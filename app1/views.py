ccd56a22e99a29b106ab5511f2917bfd6ce205d0
rs <rs@gmail.com>
2020-12-28 15:48:30 +0530

    changes in urls nd stuff

--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Albums
-from django.http import Http404
+#from django.http import Http404
 
 # Create your views here.
 
@@ -9,8 +9,6 @@ def index(request):
     return render(request, "app1/index.html", {'allAlbums':allAlbums})
 
 def details(request, id):
-    try:
-        album = Albums.objects.get(id=id)
-    except:
-        raise Http404("Album doesn't exists")
+    #album = Albums.objects.get(id=id)
+    album = get_object_or_404(Albums, id=id)
     return render(request, "app1/details.html", {'album':album})
